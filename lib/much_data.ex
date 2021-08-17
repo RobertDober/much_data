0045964e6f1082929a466d20f1349b513056e1b8
RobertDober <robert.dober@gmail.com>
2021-08-17 13:05:23 +0200

    initial commit

---
+++ lib/much_data.ex
@@ -0,0 +1,18 @@
+defmodule MuchData do
+  @moduledoc """
+  Documentation for `MuchData`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MuchData.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/much_data_test.exs
@@ -0,0 +1,8 @@
+defmodule MuchDataTest do
+  use ExUnit.Case
+  doctest MuchData
+
+  test "greets the world" do
+    assert MuchData.hello() == :world
+  end
+end
