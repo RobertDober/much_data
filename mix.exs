8e8c228aeaff63be9af7317bdbc1ca892a0ab557
RobertDober <robert.dober@gmail.com>
2021-08-24 11:41:19 +0200

    correct mix setup

--- mix.exs
+++ mix.exs
@@ -1,11 +1,22 @@
 defmodule MuchData.MixProject do
   use Mix.Project
 
+  @module "MuchData"
+
+  @version "0.1.0"
+  @url "https://github.com/RobertDober/much_data"
+
+  @description """
+  Reading and merging data from many files and many formats
+  """
+
   def project do
     [
       app: :much_data,
-      version: "0.1.0",
+      description: @description,
+      version: @version,
       elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
@@ -14,6 +25,10 @@ defmodule MuchData.MixProject do
         "coveralls.post": :test,
         "coveralls.html": :test
       ],
+
+      package: package(),
+      aliases: [docs: &build_docs/1],
+
       deps: deps()
     ]
   end
@@ -28,12 +43,55 @@ defmodule MuchData.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.25.1", only: ~w[dev]a},
       {:excoveralls, "~> 0.14.2", only: ~w[test]a},
       {:extractly, "~> 0.3.0", only: ~w[dev]a},
+      {:nested_map, "~> 0.1.1"},
       {:yaml_elixir, "~> 2.8.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp elixirc_paths(env)
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      maintainers: [
+        "Robert Dober <robert.dober@gmail.com>"
+      ],
+      licenses: [
+        "Apache-2.0"
+      ],
+      links: %{
+        "GitHub" => @url},
+    ]
+  end
+  @prerequisites """
+  run `mix escript.install hex ex_doc` and adjust `PATH` accordingly
+  """
+  defp build_docs(_) do
+    Mix.Task.run("compile")
+    ex_doc = Path.join(Mix.path_for(:escripts), "ex_doc")
+    Mix.shell().info("Using escript: #{ex_doc} to build the docs")
+
+    unless File.exists?(ex_doc) do
+      raise "cannot build docs because escript for ex_doc is not installed, make sure to \n#{
+              @prerequisites
+            }"
+    end
+
+    args = [@module, @version, Mix.Project.compile_path()]
+    opts = ~w[--main #{@module} --source-ref v#{@version} --source-url #{@url}]
+
+    Mix.shell().info("Running: #{ex_doc} #{inspect(args ++ opts)}")
+    System.cmd(ex_doc, args ++ opts)
+    Mix.shell().info("Docs built successfully")
+  end
 end
+
+#  SPDX-License-Identifier: Apache-2.0
